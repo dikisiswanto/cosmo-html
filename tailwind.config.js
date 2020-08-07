e69e73f1c1eb25f51b6cc7e233903f74f85f08fd
Diki Siswanto <dickyachmady@gmail.com>
2020-08-07 11:57:08 +0800

    add dummy sidebar panel component

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,11 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#44ACD3',
-        secondary: '#FFA630'
+        // primary: '#44ACD3',
+        primary: '#2E86AB',
+        // secondary: '#FFA630'
+        secondary: '#F18F01',
+        tertiary: '#68618F'
       },
       maxWidth: {
         container: '1200px'
