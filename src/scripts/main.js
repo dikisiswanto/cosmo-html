9ae8dafc79a5eab6b2ee59127ec8e0a6071a0274
Diki Siswanto <dickyachmady@gmail.com>
2021-06-27 17:45:58 +0800

    chore(components): create slider and card component

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,30 @@
-const a = 10;
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.swiper-container', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: true,
+    effect: 'fade',
+    fadeEffect: {
+      crossFade: true,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button.next-slide',
+      prevEl: '.swiper-button.prev-slide',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+});
